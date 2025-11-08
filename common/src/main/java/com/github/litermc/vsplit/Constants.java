2a0e077b9e11769856039f42b369dc3aac9441db
zyxkad <zyxkad@gmail.com>
2025-11-08 12:21:47 -0700

    bump version to 0.1.2

--- common/src/main/java/com/github/litermc/vsplit/Constants.java
+++ common/src/main/java/com/github/litermc/vsplit/Constants.java
@@ -6,7 +6,7 @@ import org.slf4j.LoggerFactory;
 public final class Constants {
 	public static final String MOD_ID = "vsplit";
 	public static final String MOD_NAME = "VSplit";
-	public static final String MOD_VERSION = "0.1.1";
+	public static final String MOD_VERSION = "0.1.2";
 	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
 
 	private Constants() {}
