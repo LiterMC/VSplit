af9f1f87299e4c3c9480310a54cb90dbe047a60b
zyxkad <zyxkad@gmail.com>
2025-11-24 14:53:18 -0700

    bump version to 0.3.0

--- common/src/main/java/com/github/litermc/vsplit/Constants.java
+++ common/src/main/java/com/github/litermc/vsplit/Constants.java
@@ -8,7 +8,7 @@ import org.slf4j.LoggerFactory;
 public final class Constants {
 	public static final String MOD_ID = "vsplit";
 	public static final String MOD_NAME = "VSplit";
-	public static final String MOD_VERSION = "0.2.1";
+	public static final String MOD_VERSION = "0.3.0";
 	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
 	public static final Component MESSAGE_PREFIX = Component.literal("[" + MOD_NAME + "] ").withStyle(ChatFormatting.DARK_PURPLE);
 
